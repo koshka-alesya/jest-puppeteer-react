675989ba2193d54c6af3eeca00d41a10b2f6d1ef
koshka-alesya <veronica.desmond@mail.ru>
2022-10-20 17:29:50 +0300

    step 1 - settings

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# jest-puppeteer-react
-jest-puppeteer-react
+# Automated browser tests with Jest, Puppeteer and github actions.
+
+![Automated browser tests](https://github.com/juliankrispel/jest-puppeteer-github-action-tests/workflows/Automated%20browser%20tests/badge.svg)
+
---
+++ jest-puppeteer.config.js
@@ -0,0 +1,18 @@
+console.log('hello')
+module.exports = {
+  launch: {
+    dumpio: true,
+    headless: true, //process.env.HEADLESS !== 'false',
+    executablePath: '/opt/homebrew/bin/chromium',
+    ignoreDefaultArgs: ['--disable-extensions'], 
+    args: ['--no-sandbox', "--disabled-setupid-sandbox", '--disable-gpu']
+  },
+  // browser: 'chromium',
+  // browserContext: 'default',
+  server: {
+    command: `npm start`,
+    port: 3000,
+    launchTimeout: 10000,
+    debug: true,
+  },
+}
