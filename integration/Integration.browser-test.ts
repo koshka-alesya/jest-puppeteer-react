5aa079cdad25cc0800bf3907b38bf27edefc57ec
koshka-alesya <veronica.desmond@mail.ru>
2022-10-20 21:42:49 +0300

    local fix

--- integration/Integration.browser-test.ts
+++ integration/Integration.browser-test.ts
@@ -1,14 +1,34 @@
+// for local tests
+// const puppeteer = require('puppeteer');
+
 
 describe('App', () => {
+  let browser
+  let page
   beforeAll(async () => {
+    // for local tests
+    // browser = await puppeteer.launch({
+    //   headless: false
+    // });
+    // page = await browser.newPage();
+
     await page.goto('http://localhost:3000')
+  
   })
- 
+
   it('should display a react logo', async () => {
     await expect(page).toMatch('React')
+    return true
   })
 
   it('should match a button with a "Learn React" text inside', async () => {
     await expect(page).toMatchElement('.App-link', { text: 'Learn React' })
+    return true
   })
+
+  afterAll(async () => {
+    await page.close();
+    await browser.close();
+    return true;
+  });
 })
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,16 +3,14 @@ module.exports = {
   launch: {
     dumpio: true,
     headless: true, //process.env.HEADLESS !== 'false',
-    // mac os 
-    // executablePath: '/opt/homebrew/bin/chromium',
     ignoreDefaultArgs: ['--disable-extensions'], 
     args: ['--no-sandbox', "--disabled-setupid-sandbox", '--disable-gpu']
   },
-  // comment if mac os
+
   browser: 'chromium',
   browserContext: 'default',
-  // browser: 'chromium',
-  // browserContext: 'default',
+  
+  // Comment server for local testing
   server: {
     command: `npm start`,
     port: 3000,
